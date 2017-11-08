0a0a651e735b8ddfaa3139a109d07b45b4f34097
hilmanhamidi <hilmanhamidi007@gmail.com>
2017-11-08 20:14:02 +0800

    cari nilai

--- no.5.cpp
+++ no.5.cpp
@@ -1,4 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
+
+float weight,underwight,normal,overweight;
+	int height;
+	
+	cout<<"silahkan masukan tinggi badan anda dalam cm:"<<endl;
+	cin>>height;
+	cout<<"silahkan masukan berat badan anda dalam kg:"<<endl;
+	cin>>weight;
+	
+	if(weight<height/2.5)
+	cout<<"result:underweight"<<endl;
+	else if(height/2.5<=weight<=height/2.3)
+	cout<<"result:normal"<<endl;
+	else if(height/2.3<weight)
+	cout<<"result:overweight"<<endl;
+	
+	return 0;
 }
