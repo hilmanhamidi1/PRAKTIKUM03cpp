bf2c0ae858957d027ccccde8b417309190afd414
hilmanhamidi <hilmanhamidi007@gmail.com>
2017-11-08 20:08:30 +0800

    cari nilai

--- no.4.cpp
+++ no.4.cpp
@@ -3,4 +3,16 @@
 using namespace std;
 
 int main(){
+
+   int nilai;
+   
+   cout<<"masukkan nilai:";
+   cin>>nilai;
+   
+   if(nilai>0)
+     cout<<"nilai positif";
+   else if(nilai<0)
+     cout<<"nilai negatif";
+   else
+     cout<<"nilai 0";
 }
