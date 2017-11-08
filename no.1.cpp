7352d37e20272334a068febc4b59d4c7747cc2fe
hilmanhamidi <hilmanhamidi007@gmail.com>
2017-11-08 11:52:39 +0800

    hasil fositip

--- no.1.cpp
+++ no.1.cpp
@@ -5,16 +5,24 @@
 using namespace std;
 
 int main(){
-
  int x,y,z;
  cout<<"input nilai x=";
  cin>>x;
-
-cout<<"input nilai y=";
+ 
+ cout<<"input nilai y=";
  cin>>y;
-
- if(x>y)
+ 
+ 
+ if(x>y){
   z=y-x;
   cout<<abs(z)<<endl;
   cout<<"hasil positif";
+ }
+ 
+ else if(y>x){
+  z=x-y;
+  cout<<abs(z)<<endl;
+  cout<<"hasil positif";
+ }
+ 
 }
