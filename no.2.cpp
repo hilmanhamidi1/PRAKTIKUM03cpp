14eb844a5bc51b69f2e313b4be80a222478ef981
hilmanhamidi <hilmanhamidi007@gmail.com>
2017-11-08 12:21:32 +0800

    jika salah memilih bidang

--- no.2.cpp
+++ no.2.cpp
@@ -4,10 +4,32 @@
 using namespace std;
 
 int main(){
-
 char bidang;
 double p,l,a,t,L;
 
 cout<<"pilih bidang:";
 cin>>bidang;
+
+ if(bidang=='p'||bidang=='p'){
+ 	cout<<"menghitung luas persegi panjang\n";
+ 	cout<<"masukkan panjangnya:";
+ 	cin>>p;
+ 	cout<<"masukkan lebarnya:";
+ 	cin>>l;
+ 	l=p*l;
+ 	cout<<"luas persegi panjang:"<<l<<endl;
+ }
+else if(bidang=='s'||bidang=='s'){
+	cout<<"menghitung luas segitiga\n";
+	cout<<"masukkan alasnya:";
+	cin>>a;
+	cout<<"masukkan tingginya:";
+	cin>>t;
+	l=0.5*a*t;
+	cout<<"luas persegi panjang:"<<l<<endl;
+ }
+else{
+	cout<<"maaf,anda salah memilih"<<endl;
+ }
 }
+
